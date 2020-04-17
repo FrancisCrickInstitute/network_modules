14fe3d49d54e099077f954de476b819a91312f80
Guy Morrell <guy.morrell@crick.ac.uk>
2020-04-17 15:01:38 +0100

    use standard config file

--- _format_mac.py
+++ _format_mac.py
@@ -6,11 +6,12 @@ def format_mac(mac: str) -> str:
     mac = mac.strip() # remove any whitespace
     mac = re.sub('[.:-]', '', mac).lower()  # remove delimiters and convert to lower case
     mac = ''.join(mac.split())  # remove whitespaces
-    try: 
+    try:
         assert len(mac) == 12  # length should be now exactly 12 (eg. 008041aefd7e)
+        assert mac.isalnum()  # should only contain letters and numbers
+        # convert mac in canonical form (eg. 00:80:41:ae:fd:7e)
+        mac = ":".join(["%s" % (mac[i:i+2]) for i in range(0, 12, 2)])
     except:
-        print(mac+" is not a valid MAC Address")
-    assert mac.isalnum()  # should only contain letters and numbers
-    # convert mac in canonical form (eg. 00:80:41:ae:fd:7e)
-    mac = ":".join(["%s" % (mac[i:i+2]) for i in range(0, 12, 2)])
+        print("format_mac error: INVALID MAC Address: "+mac)
+        mac = False
     return mac
--- _slack_post.py
+++ _slack_post.py
@@ -3,7 +3,14 @@ import slack # module is called slackclient
 
 # Local image taken from https://ccsearch.creativecommons.org/photos/a376def5-1f22-4e28-b2f0-8cf67398afa8
 
-OATH = 'MY_OATH_TOKEN'
+# Load global config
+with open("../network_config/slack.json") as slack_f:
+    slack_settings = json.load(slack_f)
+
+OATH = slack_settings["OAUTH_TOKEN"]
+WEBHOOK_URL = slack_settings["WEBHOOK"]
+USEWEBHOOK = slack_settings["USE_WEBHOOK"] # set 1 in ../network_config/slack.json if you can't use OATH (no graph though)
+SLACKCHANNEL = slack_settings["CHANNEL"]
 
 with open("block.json", "rt") as block_f:
     data = json.load(block_f)
@@ -11,11 +18,11 @@ with open("block.json", "rt") as block_f:
 client = slack.WebClient(token=OATH)
 
 client.files_upload(
-    channels="my-channel",
+    channels=SLACKCHANNEL,
     file="slacks.jpg",
     title="Local File"
 )
 client.chat_postMessage(
-    channel="my-channel",
+    channel=SLACKCHANNEL,
     blocks=data
 )
